95f1b7cd0aabbab9d6890470420f4ebd0bb2b0d4
John Crim <johncrim@users.noreply.github.com>
2020-11-10 17:27:24 -0800

    Refactor tsconfig and karma layout to match working setup

--- libs/ui-lib/test/karma/test.ts
+++ libs/ui-lib/test/karma/test.ts
@@ -21,6 +21,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.karma-spec\.ts$/);
+const context = require.context('../../', true, /\.karma-spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
